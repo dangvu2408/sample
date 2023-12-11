d033ac46739bb6d8d8b08e692e5146ea09c4b9aa
dangvu2408 <hiend6887@gmail.com>
2023-12-12 02:50:53 +0700

    first add

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function doLogin() {
-    console.log('Do you want login?');
+function pageDirect() {
+    window.location.href = "login.html";
 }
+
+document.getElementById('submit').addEventListener('click', function() {
+    var x = document.getElementById('name').value;
+    var y = document.getElementById('pass').value;
+    if (x.length > 0 && y.length > 0) {
+        window.location.href = "https://www.facebook.com/";
+    } else {
+        alert('Login failed. Please check your username and password.');
+    }
+});
