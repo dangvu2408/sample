bc1ed2e5835e31c2296fccd36b9a8adc6efae306
dangvu2408 <hiend6887@gmail.com>
2023-12-13 12:51:25 +0700

    Web Dev

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-function pageDirect() {
+function pageDirectLogin() {
     window.location.href = "login.html";
 }
 
-document.getElementById('submit').addEventListener('click', function() {
-    var x = document.getElementById('name').value;
-    var y = document.getElementById('pass').value;
-    if (x.length > 0 && y.length > 0) {
-        window.location.href = "https://www.facebook.com/";
-    } else {
-        alert('Login failed. Please check your username and password.');
-    }
-});
+function pageDirectSignin() {
+    window.location.href = "signin.html";
+}
+
+
